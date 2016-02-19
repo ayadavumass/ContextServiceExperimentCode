c683b079ebec327ef0410ba4be344dc5eaf382ab
Aditya <ayadav@cs.umass.edu>
2016-02-19 17:15:28 -0500

    changed to location updates

--- src/edu/umass/cs/weatherExpClient/WeatherAndMobilityBoth.java
+++ src/edu/umass/cs/weatherExpClient/WeatherAndMobilityBoth.java
@@ -164,13 +164,13 @@ public class WeatherAndMobilityBoth
 		long end = System.currentTimeMillis();
 		System.out.println(numUsers+" initialization complete "+(end-start));
 		
-		LocationUpdateFixedUsers locUpdate = null;
-		//LocationUpdateFixedMovingUsers locUpdate = null;
+		//LocationUpdateFixedUsers locUpdate = null;
+		LocationUpdateFixedMovingUsers locUpdate = null;
 		UniformQueryClass searchQClass = null;
 		
 		if(updateEnable)
 		{
-			locUpdate = new LocationUpdateFixedUsers();
+			locUpdate = new LocationUpdateFixedMovingUsers();
 			new Thread(locUpdate).start();
 		}
 		if(searchEnable)
