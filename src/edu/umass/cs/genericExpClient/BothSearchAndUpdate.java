83d1917b59b85ebba6871433c502905c6bb90b82
Aditya <ayadav@cs.umass.edu>
2016-06-04 15:28:36 -0400

    changed back to 30% attr range

--- src/edu/umass/cs/genericExpClient/BothSearchAndUpdate.java
+++ src/edu/umass/cs/genericExpClient/BothSearchAndUpdate.java
@@ -225,8 +225,8 @@ public class BothSearchAndUpdate extends
 //				// reinitialize rand number so that it gives the same seq again.
 //				searchQueryRand = new Random(SearchAndUpdateDriver.myID*200);
 //			}
-			sendQueryMessage();
-			//sendQueryMessageWithSmallRanges();
+			//sendQueryMessage();
+			sendQueryMessageWithSmallRanges();
 		}
 		else
 		{
