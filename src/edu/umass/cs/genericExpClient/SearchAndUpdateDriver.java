d506c321fbd1568280d251eacba893f17992eb8f
Aditya <ayadav@cs.umass.edu>
2016-06-17 11:00:15 -0400

    changed the number of threads from 200 to 2000

--- src/edu/umass/cs/genericExpClient/SearchAndUpdateDriver.java
+++ src/edu/umass/cs/genericExpClient/SearchAndUpdateDriver.java
@@ -161,7 +161,7 @@ public class SearchAndUpdateDriver
 			new Thread( new ReadTriggerRecvd() ).start();
 		}
 		
-		taskES = Executors.newFixedThreadPool(200);
+		taskES = Executors.newFixedThreadPool(2000);
 		
 		if( userInitEnable )
 		{
