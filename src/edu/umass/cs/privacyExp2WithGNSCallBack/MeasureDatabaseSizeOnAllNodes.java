af9bd674bb950b0da668bb4710565b6eda055343
Aditya Yadav <ayadav@cs.umass.edu>
2016-10-25 17:46:32 -0400

    Added database measurement java code

--- src/edu/umass/cs/privacyExp2WithGNSCallBack/MeasureDatabaseSizeOnAllNodes.java
+++ src/edu/umass/cs/privacyExp2WithGNSCallBack/MeasureDatabaseSizeOnAllNodes.java
@@ -13,37 +13,36 @@ public class MeasureDatabaseSizeOnAllNodes
 		int numNodes = Integer.parseInt(args[0]);
 		double totalSize = 0;
 		
-		for( int i=0; i < numNodes; i++ )
-		{
-			// du -h /home/mysqlDir-serv0/ | tail -1
-			String command = "/proj/MobilityFirst/ayadavDir/contextServiceScripts/mysqlSizeBash.sh";
-			System.out.println("Command "+command);
-			Process p = Runtime.getRuntime().exec(command);
-		    p.waitFor();
-		    
-		    BufferedReader reader =
-		         new BufferedReader( new InputStreamReader(p.getInputStream()) );
-		    
-		    String line = "";
-		    
-		    while ( (line = reader.readLine())!= null )
-		    {
-		    	System.out.println("serv"+i+" "+line);
-		    	String[] parsed = line.split(" ");
-		    	String sizeString = parsed[0].trim();
-		    	
-		    	if( sizeString.charAt(sizeString.length()-1) == 'M' )
-		    	{
-		    		String numericValString = sizeString.substring(0, sizeString.length()-1);
-		    		
-		    		totalSize = totalSize + Double.parseDouble(numericValString);
-		    	}
-		    	else
-		    	{
-		    		assert(false);
-		    	}
-		    }
-		}	
+		// du -h /home/mysqlDir-serv0/ | tail -1
+		String command = "/proj/MobilityFirst/ayadavDir/contextServiceScripts/mysqlSizeBash.sh";
+		System.out.println("Command "+command);
+		Process p = Runtime.getRuntime().exec(command);
+	    p.waitFor();
+	    
+	    BufferedReader reader =
+	         new BufferedReader( new InputStreamReader(p.getInputStream()) );
+	    
+	    String line = "";
+	    
+	    while ( (line = reader.readLine())!= null )
+	    {
+	    	System.out.println(line);
+	    	String[] parsed = line.split(" ");
+	    	
+	    	String sizeString = parsed[0].trim();
+	    	System.out.println(" parse[0] "+parsed[0] +" "+parsed[1]+" sizeString "+sizeString);
+	    	if( sizeString.charAt(sizeString.length()-1) == 'M' )
+	    	{
+	    		String numericValString = sizeString.substring(0, sizeString.length()-1);
+	    		
+	    		totalSize = totalSize + Double.parseDouble(numericValString);
+	    	}
+	    	else
+	    	{
+	    		assert(false);
+	    	}
+	    }
+			
 		System.out.println("Total database size "+totalSize);
 	}
 	
