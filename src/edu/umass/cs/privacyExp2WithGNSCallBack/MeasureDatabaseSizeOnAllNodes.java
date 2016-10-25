00203f0e008fe19bd1e6af25b46530139245bffd
Aditya Yadav <ayadav@cs.umass.edu>
2016-10-25 17:17:39 -0400

    Added database measurement java code

---
+++ src/edu/umass/cs/privacyExp2WithGNSCallBack/MeasureDatabaseSizeOnAllNodes.java
@@ -0,0 +1,51 @@
+package edu.umass.cs.privacyExp2WithGNSCallBack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MeasureDatabaseSizeOnAllNodes
+{
+	public static final String NODE_NAME_PREFIX			= "serv";
+	
+	public static void main( String[] args ) throws IOException, InterruptedException
+	{
+		int numNodes = Integer.parseInt(args[0]);
+		double totalSize = 0;
+		
+		for( int i=0; i < numNodes; i++ )
+		{
+			// du -h /home/mysqlDir-serv0/ | tail -1
+			String command = "du -h /home/mysqlDir-serv"+i+" | tail -1";
+			
+			Process p = Runtime.getRuntime().exec(command);
+		    p.waitFor();
+		    
+		    BufferedReader reader =
+		         new BufferedReader( new InputStreamReader(p.getInputStream()) );
+		    
+		    String line = "";
+		    
+		    while ( (line = reader.readLine())!= null )
+		    {
+		    	System.out.println("serv"+i+" "+line);
+		    	String[] parsed = line.split(" ");
+		    	String sizeString = parsed[0].trim();
+		    	
+		    	if( sizeString.charAt(sizeString.length()-1) == 'M' )
+		    	{
+		    		String numericValString = sizeString.substring(0, sizeString.length()-1);
+		    		
+		    		totalSize = totalSize + Double.parseDouble(numericValString);
+		    	}
+		    	else
+		    	{
+		    		assert(false);
+		    	}
+		    }
+		}
+		
+		System.out.println("Total database size "+totalSize);
+	}
+	
+}
